b5332947a878c0942898f9c4fecb93c6776d0647
Sulu Edward Julianto <93524287+SuluJulianto@users.noreply.github.com>
2023-12-02 01:41:16 +0700

    Create hello-world.cpp

---
+++ my-first-code-in-cpp/hello-world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World";
+    cout<<"\nI'm Sulu\n";
+    cout<<"This is my First Program in C++\n";
+    cout <<"Thank You!!!";
+    return 0;
+}

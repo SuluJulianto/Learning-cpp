fe31774f7e8e1e194631c8e98e3181382235f5d2
Sulu Edward Julianto <93524287+SuluJulianto@users.noreply.github.com>
2023-12-02 07:53:22 +0700

    update hello world

--- my-first-code-in-cpp/hello-world/hello-world.cpp
+++ my-first-code-in-cpp/hello-world/hello-world.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     cout<<"Hello, World";
-    cout<<"\nI'm Sulu\n";
+    cout<<"\nI'm Sulu Edward Julianto\n";
     cout<<"This is my First Program in C++\n";
     cout <<"Thank You!!!";
     return 0;
